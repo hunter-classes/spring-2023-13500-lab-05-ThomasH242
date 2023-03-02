dbb06c62c1fab878ee559849cfeda37803e8e185
ThomasH242 <Thomas.Huang45@myhunter.cuny.edu>
2023-03-01 22:34:49 -0500

    3/1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
-
+//Thomas Huang, ThomasH242
 #include "funcs.h"
 
-int main()
-{
+int main(){
+  std::cout << "Prime: " << "\n";
+  for(int i = 0; i <= 100;i++){
+    if(isPrime(i)){
+        std::cout << i << " ";
+      }
+  }
+  std::cout << "\n";
+  std::cout << "TwinPrime: " << "\n";
+  for(int j = 0; j <= 100;j++){
+    if(isTwinPrime(j)){
+        std::cout << j << " ";
+      }
+  }
+  std::cout << "\n";
   return 0;
 }
